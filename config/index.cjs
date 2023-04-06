04032d03ef363cefcfe6e37833113d4ef1d0bc07
Eric-Tianzh <90679304+Eric-Tianzh@users.noreply.github.com>
2023-04-06 22:29:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -37,7 +37,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小王',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o9Bzu6i0KqjHa4BppJvyNTkIdmLw',
+      id: 'o9Bzu6u2ZUOc8bmWr_4EV1M2y0Mk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'EaY-12oFE4Zz24hIXbBF63F9z_qsYe5AXiem_Cfqigk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
